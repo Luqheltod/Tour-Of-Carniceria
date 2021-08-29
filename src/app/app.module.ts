c4b72e1c147e636b825d69a2d4bda9a54636b23c
German Sanchez Pastor <luqh555@gmail.com>
2021-08-29 20:13:50 +0200

    Creacion de componente productos-detalle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
+import { ProductosDetalleComponent } from './productos-detalle/productos-detalle.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductosComponent,
+    ProductosDetalleComponent,
    
   ],
   imports: [
