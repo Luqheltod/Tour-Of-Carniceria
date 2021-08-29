23db66ad84ad13a7397552889d33c210c45fd5f5
German Sanchez Pastor <luqh555@gmail.com>
2021-08-29 19:02:41 +0200

    Mock Productos creados

---
+++ src/app/modelos/productos-muestra.ts
@@ -0,0 +1,40 @@
+import { Producto } from "./producto";
+
+export const PRODUCTOS : Producto[] = [
+    {
+        id: 1,
+        nombre : 'Salchichon de Cocodrilo',
+        descripcion : "100% hecho unicamente de cocodrilos que comen cocodrilos que comen cocodrilos que comen personas",
+        origen : "Egipto"
+    } ,
+    {
+        id: 2,
+        nombre : 'Mortadela de Fenix',
+        descripcion : "Revive en tu estomago",
+        origen : "Alburquerque"
+    } ,
+    {
+        id: 3,
+        nombre : 'Pinchito de Tomberi',
+        descripcion : "Cactus vivientes que aparecen espontaneamente son seleccionados cuidadosamente",
+        origen : "Peñiscola"
+    } ,
+    {
+        id: 4,
+        nombre : 'Tostadito de Pikachu',
+        descripcion : "100% hecho unicamente de cocodrilos que comen cocodrilos que comen cocodrilos que comen personas",
+        origen : "Costa de Marfil"
+    } ,
+    {
+        id: 5,
+        nombre : 'Panacea de elixir',
+        descripcion : "100% hecho con virutas del santo grial y pelo de goku",
+        origen : "Madagascar"
+    } ,
+    {
+        id: 6,
+        nombre : 'Jamon de Papaya',
+        descripcion : "De papayas criadas en libertad",
+        origen : "Ruralandia"
+    } 
+];
